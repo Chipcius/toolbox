ffd986f5841c2e3302b22093a934e96d972057da
Andri Thorlacius <andri.thorlacius@gmail.com>
2018-11-28 13:16:52 +0000

    Added base install script

---
+++ ubuntu-post-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install shutter git neovim curl type curl  make htop glances iftop iotop indicator-multiload openssh-server krita
+sudo snap install sublime-text --classic
+sudo snap install slack --classic
+sudo snap install google-cloud-sdk --classic
+sudo snap install kubectl --classic
