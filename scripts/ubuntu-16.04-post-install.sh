81c239f283d1edcb30bbccdee64ee0fc8c327083
Andri Thorlacius <andri.thorlacius@gmail.com>
2018-11-28 16:07:42 +0000

    Fixed with regards to 16.04 install

--- scripts/ubuntu-16.04-post-install.sh
+++ scripts/ubuntu-16.04-post-install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-sudo apt-get install shutter git neovim curl type curl  make htop glances iftop iotop indicator-multiload openssh-server krita
+sudo apt-get install \
+  shutter\
+  git\
+  vim\
+  curl\
+  make\
+  htop\
+  glances\
+  iftop\
+  iotop\
+  indicator-multiload\
+  openssh-server\
+  krita
+
 sudo snap install sublime-text --classic
 sudo snap install slack --classic
 sudo snap install google-cloud-sdk --classic
